9c10eec8ea91302d6782c3040832da83b7c9e4df
alphi92 <alphi922@gmail.com>
2020-08-19 09:44:04 +0200

    Updates

---
+++ precache-manifest.5d7ed3cdbee38aebb6ddd37c4f558067.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f58cc8927a0e3a71ec0431a5e5e269bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe1ff919b33ca05ecbd3",
+    "url": "/static/css/main.195c8d4c.chunk.css"
+  },
+  {
+    "revision": "79e75030d8fc5260393c",
+    "url": "/static/js/2.0ecad26f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0ecad26f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe1ff919b33ca05ecbd3",
+    "url": "/static/js/main.dbb8b41d.chunk.js"
+  },
+  {
+    "revision": "d35ec7f789a707a74df9",
+    "url": "/static/js/runtime-main.f63608df.js"
+  }
+]);
