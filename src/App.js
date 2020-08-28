0b9ec69b641bc59abafdd5b757617d0b41b960be
alphi92 <alphi922@gmail.com>
2020-08-28 18:27:16 +0200

    Added hamburger menu, logo and styling site.

--- src/App.js
+++ src/App.js
@@ -5,38 +5,57 @@ import ListContacts from "./components/ListContacts"
 import DelContacts from "./components/DelContacts"
 import EditContacts from "./components/EditContacts"
 import Search from "./components/Search"
+import calling from "./calling.jpg"
+
 
 
 
 import  "./App.css"
 
+
+
 class App extends Component{
+  
+
+  handleClick = () => {
+    const hamburger = document.querySelector('.hamburger');
+    const nav = document.querySelector('.navigation');
+    hamburger.classList.toggle('hamburger--active');
+    nav.classList.toggle('navigation--active');
+  }
 
   render(){
 
+    
     return(
+      
+      
 
       <div className="stronaGlowna">
+
+        <div className="logo"><img src={calling} alt="logo"/></div>
+        
         
           <h1>Książka Telefoniczna</h1> 
           
-          <div class="box4"></div>
-          <div class="box5"></div>
-          <div class="box6"></div>
-         
+          <button class="hamburger" onClick={this.handleClick}>
+            <span class="hamburger__box">
+              <span class="hamburger__inner"></span>
+            </span> 
+          </button>
+          
+          
           
         
         <BrowserRouter>
 
-          
-            
-        <div class="topnav">
-              <Link to="/">Lista kontaktów</Link>
-            
-              <Link to="/add">Dodaj nowy kontakt</Link>
-            
-              <Link to="/search">Wyszukaj kontakt</Link>
-         </div>   
+        <div class="navigation">
+          <ul class="navigation__list">
+            <li class="navigation__item"><Link to="/">Lista kontaktów</Link></li>
+            <li class="navigation__item"><Link to="/add">Dodaj nowy kontakt</Link></li>
+            <li class="navigation__item"><Link to="/search">Wyszukaj kontakt</Link></li>
+          </ul> 
+         </div> 
         
           <Switch>
             <Route exact path="/" component={ListContacts} />
@@ -48,11 +67,14 @@ class App extends Component{
           
         </BrowserRouter>
         
-
+        <footer>
+        <p>© 2020 Damian Skiba @alphonso92</p>
+    </footer>
+      
       </div>
-
+    
     );
-
+    
   }
 
 }
